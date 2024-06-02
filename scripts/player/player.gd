f48d2735a7917cc7ca30884f53bd240291c60a49
mateusrosa <pedrompmrosa@gmail.com>
2024-06-02 23:53:37 +0100

    working

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -8,9 +8,9 @@ var jump_count: int = 0
 var landing: bool = false
 @export var jump_speed: int
 @export var player_gravity: int
-var attacking : bool = false 
-var blocking : bool = false
-var crouching : bool = false
+var is_attacking : bool = false 
+var is_blocking : bool = false
+var is_crouching : bool = false
 var can_track_input : bool = true;
 
 
@@ -18,18 +18,24 @@ var can_track_input : bool = true;
 func _physics_process(delta: float):
 	horizontal_movement_env()
 	vertical_movement_env()
+	actions_env()
 	move_and_slide()
 	gravity(delta)
 	player_sprite.animate(velocity)
 		
 func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if not can_track_input or is_attacking:
+		velocity.x = 0
+		return
+		
 	velocity.x = input_direction * speed
 
 func vertical_movement_env():
 	if is_on_floor():
 		jump_count = 0
-	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+	var jump_condition = can_track_input and not is_attacking
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and jump_condition:
 		jump_count += 1
 		velocity.y = jump_speed
 
@@ -38,3 +44,33 @@ func gravity(delta:float) -> void:
 	if velocity.y >= player_gravity:
 		velocity.y = player_gravity
 
+func actions_env() -> void :
+	attack()
+	crouch()
+	block()
+	
+func attack() -> void :
+	var attack_condition: bool = not is_attacking and not is_crouching and not is_blocking
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		is_attacking = true
+		player_sprite.normal_attack = true
+		
+func crouch() -> void :
+	if Input.is_action_pressed("crouch") and is_on_floor() and not is_blocking:
+		is_crouching = true
+		can_track_input = false
+	elif not is_blocking:
+		is_crouching = false
+		can_track_input = true
+		player_sprite.crouching_off = true
+			
+	
+func block() -> void :
+	if Input.is_action_pressed("block") and is_on_floor() and not is_crouching:
+		is_blocking = true
+		can_track_input = false
+	elif not is_crouching:
+		is_blocking = false
+		can_track_input = true
+		player_sprite.shield_off = true
+	
--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -5,7 +5,10 @@ class_name Sprite
 @onready var animation : AnimationPlayer = get_node(animation_path)
 @export var player_path: NodePath
 @onready var player: CharacterBody2D = get_node(player_path)
-
+var normal_attack:bool = false;
+var shield_off: bool = false
+var crouching_off:bool = false
+var suffix:String = "_right"
 
 func _ready() -> void:
 # Check if the nodes are valid
@@ -19,7 +22,10 @@ func _ready() -> void:
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
 	
-	if  direction.y < 5.83333349225 || direction.y > 5.833334:
+	if player.is_attacking or player.is_blocking or player.is_crouching:
+		print(player.is_blocking)
+		action_behaviour()
+	elif  direction.y < 5.83333349225 || direction.y > 5.833334:
 		vertical_behaviour(direction)
 	elif player.landing:
 		player.set_physics_process(false)
@@ -31,9 +37,24 @@ func animate(direction: Vector2) -> void:
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
 		flip_h = false
+		suffix = "_right"
 	elif direction.x < 0:
 		flip_h = true
+		suffix = "_left"
 		
+func action_behaviour() -> void:
+	
+	if player.is_attacking == true and normal_attack:
+		print("bananas")
+		animation.play("attack" + suffix)
+	elif player.is_blocking and shield_off:
+		animation.play("block")
+		shield_off = false
+	elif player.is_crouching and crouching_off:
+		animation.play("crouch")
+		crouching_off = false
+		
+
 func vertical_behaviour(direction: Vector2) -> void:
 	if direction.y > 6:
 		animation.play("fall")
@@ -52,3 +73,9 @@ func _on_animation_finished(anim_name: String):
 		"landing":
 			player.landing = false
 			player.set_physics_process(true)
+		"attack_left":
+			normal_attack = false
+			player.is_attacking = false
+		"attack_right":
+			player.is_attacking = false
+			normal_attack = false
